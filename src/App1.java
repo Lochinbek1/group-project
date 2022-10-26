1f6b8b0de28495278b3781e5223ed4d904d9b6f3
Tohir <ismailovtohir4349@gmail.com>
2022-10-26 15:16:38 +0500

    test

--- src/App1.java
+++ src/App1.java
@@ -2,5 +2,6 @@ public class App1 {
     public static void main(String[] args) {
         System.out.println("Salom Toxir aka.");
         System.out.println("Zo'rku uka");
+        System.out.println("Alik");
     }
 }
